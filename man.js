5662b05cc945f0bd503bba9f31fff216d9b1c1f2
Ivan1243 <58366420+Ivan1243@users.noreply.github.com>
2020-07-17 09:54:17 +0300

    Update man.js

--- man.js
+++ man.js
@@ -1,3 +1,25 @@
+
+class Man {
+    constructor (name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    info () {
+        if (this.gender) {
+            console.log(' Это мужчина ' + 'ему ' + this.age +  ' лет ' + 'его зовут ' + this.name );
+        }
+        else {
+            console.log(' Это женщина ' + 'ей ' + this.age +  ' лет ' + 'её зовут ' + this.name );
+    }
+}
+}
+var man = new Man('Alex', 14, true);
+var woman = new Man('Mary', 15, false);
+man.info();
+woman.info();
+
+
 class Thing {
     constructor (name, type, color){
         this.name = name;
@@ -10,30 +32,6 @@ class Thing {
 }
 var t = new Thing("мяч", "футбольный", "чёрный и белый");
 t.info();
-
-
-
-class Man {
-    constructor (name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-    info () {
-        if (this.gender) {
-            console.log(' Это мужчина  ' + 'ему ' + this.age +  ' лет ' + 'его зовут ' + this.name );
-        }
-        else {
-            console.log(' Это женщина  ' + 'ей ' + this.age +  ' лет ' + 'её зовут ' + this.name );
-    }
-}
-}
-var man = new Man('Alex', 14, true);
-var woman = new Man('Mary', 15, false);
-man.info();
-woman.info();
-
-
 
 
 
@@ -50,34 +48,31 @@ class Classmate extends Man {
     }
 //    SortedArray () {}
     
-    addThing(t) {
-        this.things.push(t);
-    }
+//    addThing(t) {
+//        this.things.push(t);
+//    }
 }
 var cm = new Classmate('Jack', 13, true, [new Thing('мяч', 'футбольный', 'белый'), new Thing('компьютер', 'техника', 'чёрный')])
+var cm2 = new Classmate('Alice', 14, false, [new Thing('шар', 'воздушный', 'синий'), new Thing('ручка', 'шариковая', 'чёрная')])
 cm.info();
+cm2.info();
 //cm.addThing(new Thing['мяч', 'баскетбольный', 'коричневый']);
 
 
-
 class Class {
-    constructor(name, cmname, lateness) {
+    constructor(name, cm, tname) {
         this.name = name;
-        this.cmname = cmname;
-        this.lateness = lateness;
+        this.cm = cm;
+        this.tname = tname;
     }
     
- info() {
-        if (this.lateness) {
-            console.log(' Опоздал/а ');
-        }
-        else {
-            console.log(' Не опоздал/а ');
-    }
-          console.log(" В классе " + this.cmname.length + " человек: "   )
-        for (var i = 0; i < this.cmname.length; i++) { 
-            console.log(this.cmname[i]) }
+    info() {
+          console.log(" В " + this.name + " классе " + this.cm.length + " человек: "   )
+        for (var i = 0; i < this.cm.length; i++) {
+            console.log(this.cm[i]) }
     }
 }
-var m = new Class('7C', ['Mary', 'Gen', 'Santa', 'Andrei', 'John', 'Tom', 'Bobby'], [false, false, true, false, false, true, false] )
-.info()
+var c = new Class('7C', [new Classmate('Jack', 13, true, new Thing('мяч', 'футбольный', 'белый')), new Classmate('Alice', 14, false, new Thing('шар', 'воздушный', 'синий'))], 'Polina Andreevna') 
+c.info()
+
+
